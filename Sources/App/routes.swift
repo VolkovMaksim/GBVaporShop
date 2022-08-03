37dbfe339a24ef28a05ddbc46e9664366ab260ff
Maksim Volkov <volkovmd.dev@mail.ru>
2022-08-03 18:11:27 +0300

    Add sales

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,11 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    //MARK: ПРОДАЖА
+    let controllerSale = SaleController()
+    // при запросе на url "sale" будет использован класс SaleController - метод sale
+    app.post("sale", use: controllerSale.sale)
 
     try app.register(collection: TodoController())
 }
